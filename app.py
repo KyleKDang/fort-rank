e4d1e52ffd422994d50b534c425157d1f35cedb5
Kyle Dang <kyledang@Kyles-MacBook-Air.local>
2024-09-13 17:28:38 -0700

    Test website structure with sample players

--- app.py
+++ app.py
@@ -9,7 +9,22 @@ def index():
     cursor = conn.cursor()
 
     cursor.execute('SELECT * FROM players ORDER BY rank ASC')
-    players = cursor.fetchall()
+    player_rows = cursor.fetchall()
+
+    players = []
+
+    for row in player_rows:
+        player = {
+            'id': row[0],
+            'username': row[1],
+            'name': row[2],
+            'date_of_birth': row[3],
+            'country': row[4],
+            'total_earnings': row[5],
+            'rank': row[6],
+            'image_url': row[7]
+        }
+        players.append(player)
 
     conn.close()
     return render_template("index.html", players=players)
